86fef91250e69ac4b40e8a5751f4c295d6026506
Hamilton Clark <dhclark2@ncsu.edu>
2016-12-15 14:05:55 -0500

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+tweets = Tweet.create(
+  [
+    { tweet: 'Kanye is crazy', user_name: 'hamilton', link: 'https://en.wikipedia.org/wiki/2009_MTV_Video_Music_Awards' },
+    { tweet: 'I learned about Kanye', user_name: 'clarktastic', link: 'https://en.wikipedia.org/wiki/Kanye_West' },
+    { tweet: 'This is where to search', user_name: 'hamilton', link: 'https://www.google.com/' }
+  ]
+)
